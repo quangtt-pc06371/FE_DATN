dc40762e5aabe4bba15b30b4accabcea5838504e
tronghieu2512 <141827099+tronghieu2512@users.noreply.github.com>
2024-11-09 13:19:17 +0700

    push app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React,{ useEffect }  from 'react';
+import React, { useEffect } from 'react';
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
+import QuanlySanPham from './components/pages/Quanlysanpham'
+import DanhSachSanPham from './components/pages/Danhsachsanpham';
+import QuanLySanPhamKhuyenMai from './components/pages/Quanlysanphamkhuyenmai';
+import DanhSachSanPhamKM from './components/pages/Danhsachchuongtrinhkm';
+import TrangChu from './components/pages/TrangChu'
+import SanPham from './components/pages/Sanpham';
+import ChiTietSanPham from './components/pages/Chitietsanpham';
 import NavbarAdmin from './components/pages/admin/NavbarAdmin';
 import CategoryManagement from './components/pages/admin/CategoryManagement';
 import ShopApproval from './components/pages/admin/ShopApproval';
@@ -14,99 +26,60 @@ import ShopUser from './components/pages/user/ShopUser';
 import Register from "./components/pages/Register";
 import Profile from "./components/pages/Profile/index";
 import Login from "./components/pages/Login";
- import Updateuser2 from './components/compoments/updatetaikhoan';
- import Listtk from './components/compoments/Listtaikhoan';
- import Registergg from "./components/pages/logingoogle";
+import Updateuser2 from './components/compoments/updatetaikhoan';
+import Listtk from './components/compoments/Listtaikhoan';
+import Registergg from "./components/pages/logingoogle";
 //  import {YourComponent} from './components/pages/Refe';
- 
- import { startTokenRefreshInterval } from "./components/pages/Refresh";
+
+import { startTokenRefreshInterval } from "./components/pages/Refresh";
 
 // import Dangnhap from "./components/pages/Login/App";
-function App() {
-  // useEffect(() => {
-  //   const checkTokenExpiry = () => {
-  //     const token = Cookies.get('token');
-  //     if (token==null) {
-  //       console.error("Không tìm thấy token trong cookie.");
-  //       return true; // Nếu không có token, cần làm mới
-  //     }
-    
-  //     try {
-  //       const decoded = jwtDecode(token); // Sử dụng jwtDecode thay vì jwt_decode
-  //       const now = Date.now() / 1000; // Lấy thời gian hiện tại tính bằng giây
-    
-  //       return decoded.exp ? now >= decoded.exp : true; // Kiểm tra nếu token đã hết hạn
-  //     } catch (error) {
-  //       console.error("Lỗi khi giải mã token:", error);
-  //       return true; // Nếu lỗi khi giải mã, cần làm mới token
-  //     }
-  //   };
-  //   const refreshToken = async () => {
-  //     const refreshToken = Cookies.get('refreshToken');
-  //     if (!refreshToken) {
-  //       console.error('Không tìm thấy refresh token');
-  //       return;
-  //     }
-    
-  //     try {
-  //       const response = await axios.post('http://localhost:8080/api/auth/refresh', { refreshToken });
-  //       const newAccessToken = response.data.token;
-    
-  //       // Cập nhật access token mới vào cookie, với thời gian hết hạn là 1 ngày
-  //       Cookies.set('token', newAccessToken, { expires: 1 });
-  //       console.log("Token đã được làm mới thành công");
-  //     } catch (error) {
-  //       console.error("Làm mới token thất bại:", error);
-  //       // Điều hướng người dùng đến trang đăng nhập nếu token không thể làm mới
-  //       window.location.href = '/login';
-  //     }
-  //   };
-  //   // Thiết lập để tự động làm mới token mỗi 2 phút (120000 ms)
-  //   const interval = setInterval(() => {
-  //     if (checkTokenExpiry()) {
-  //       refreshToken().catch(error => {
-  //         console.error('Làm mới token thất bại:', error);
-  //         window.location.href = '/login'; // Điều hướng người dùng đến trang đăng nhập
-  //       });
-  //     }
-  //   }, 1 * 60 * 1000); // 2 phút
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<TrangChu />}>
+          <Route path="/category-management" element={<CategoryManagement />} />
+          <Route path="/shop-approval" element={<ShopApproval />} />
+          <Route path="/category" element={<CategoryManagement />} />
+          <Route path="/shop-management" element={<ShopManagement />} />
 
-  //   // // // Dọn dẹp khi component unmount
-  //   return () => clearInterval(interval);
+          <Route path="/shop-register" element={<ShopRegistration />} />
 
-  //   // startTokenRefreshInterval();
-  //   // YourComponent();
-  // }, []);
+          <Route path="/shop-user" element={<ShopUser />} />
+
+          <Route path='/' element={<SanPham />} />
+          <Route path='/chitietsanpham/:id' element={<ChiTietSanPham />} />
+
+          {/* <Route path="app" element={<Dangnhap/>} /> */}
+          <Route path="login" element={<Login />} />
+
+          <Route path="listtaikhoan" element={<Listtk />} />
+          <Route path="updateuser" element={<Updateuser2 />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="register" element={<Register />} />
+          <Route path="registergg" element={<Registergg />} />
+
+          <Route path='/quanlysanpham' element={<QuanlySanPham />} />
+          <Route path='/sanpham/:idSanPham' element={<QuanlySanPham />} />
+          <Route path='/danhsachsanpham' element={<DanhSachSanPham />} />
+          <Route path='/danhsachsanphamkhuyenmai' element={<DanhSachSanPhamKM />} />
+
+          <Route path='/sanphamkhuyenmai' element={<QuanLySanPhamKhuyenMai />} />
+          <Route path='/sanphamkhuyenmai/:idSanPhamKhuyenMai' element={<QuanLySanPhamKhuyenMai />} />
+        </Route>
+
+
+
+
+      </Route>
+    )
+  );
+
 
   return (
-    
-    <Router>
-      
-      <NavbarAdmin />
-      
-      <Routes>
-        <Route path="/category-management" element={<CategoryManagement />} />
-        <Route path="/shop-approval" element={<ShopApproval />} />
-        <Route path="/" element={<CategoryManagement />} />
-        <Route path="/shop-management" element={<ShopManagement />} />
-
-        <Route path="/shop-register" element={<ShopRegistration />} />
-
-        <Route path="/shop-user" element={<ShopUser />} />
-
-
-        {/* <Route path="app" element={<Dangnhap/>} /> */}
-        <Route path="login" element={<Login />} />
-       
-        <Route path="listtaikhoan" element={<Listtk />} />
-        <Route path="updateuser" element={<Updateuser2 />} />
-        <Route path="profile" element={<Profile/>} />
-        <Route path="register" element={<Register/>} />
-        <Route path="registergg" element={<Registergg/>} />
-      </Routes>
-
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
-
-export default App;
