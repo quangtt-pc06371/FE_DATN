2fe48b33595a382571b7296dc479893298666bf8
tronghieu2512 <141827099+tronghieu2512@users.noreply.github.com>
2024-11-14 11:39:00 +0700

    hieunt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  RouterProvider,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements
+} from 'react-router-dom'
 import NavbarAdmin from './components/pages/admin/NavbarAdmin';
 import CategoryManagement from './components/pages/admin/CategoryManagement';
 import ShopApproval from './components/pages/admin/ShopApproval';
@@ -10,25 +15,77 @@ import ShopRegistration from './components/pages/user/ShopRegistration';
 import DangKy from "./components/compoments/Addtaikhoan";
 import Profile from "./components/pages/Profile/index";
 import Login from "./components/pages/Login";
+import TrangChu from "./components/pages/TrangChu";
+import SanPham from './components/pages/Sanpham';
+import ChiTietSanPham from './components/pages/Chitietsanpham';
+import QuanLykhuyenMai from './components/pages/Quanlykhuyenmai';
+import DanhSachkhuyenMai from './components/pages/Danhsachkhuyenmai';
+import QuanlySanPham from './components/pages/Quanlysanpham';
+import DanhSachSanPham from './components/pages/Danhsachsanpham';
+import DanhSachSanPhamKM from './components/pages/Danhsachchuongtrinhkm';
+import QuanLySanPhamKhuyenMai from './components/pages/Quanlysanphamkhuyenmai';
+// function App() {
+//   return (
+//     <Router>
+//       <NavbarAdmin />
+//       <Routes>
+//         <Route path="/category-management" element={<CategoryManagement />} />
+//         <Route path="/shop-approval" element={<ShopApproval />} />
+//         <Route path="/" element={<CategoryManagement />} />
+//         <Route path="/shop-management" element={<ShopManagement />} />
+//         <Route path="/shop-register" element={<ShopRegistration />} />
 
-function App() {
+//         <Route path="login" element={<Login />} />
+//         <Route path="profile" element={<Profile />} />
+//         <Route path="dangky" element={<DangKy />} />
+//       </Routes>
+
+//     </Router>
+//   );
+// }
+
+
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<TrangChu />}>
+        
+         
+        
+          {/* <Route path='/b' element={<QuanLyHoaDon />} /> */}
+
+          {/* <Route path='/c' element={<HoSoCaNhan />} /> */}
+          {/* <Route path='/d' element={<QuanLyHoaDonDaMua />} /> */}
+
+          <Route path='/' element={<SanPham />} />
+          <Route path='/chitietsanpham/:id' element={<ChiTietSanPham />} />
+
+          <Route path="/khuyenmai" element={<QuanLykhuyenMai />} />
+          <Route path='/khuyenmai/:idKhuyenMai' element={<QuanLykhuyenMai />} />
+          <Route path='/danhsachkhuyenmai' element={<DanhSachkhuyenMai />} />
+
+          {/* <Route path="/nhacungcap" element={<NhaCungCap />} />
+          <Route path='/nhacungcap/:idNhaCungCap' element={<NhaCungCap />} />
+          <Route path='/danhsachnhacungcap' element={<QuanLyNhaCungCap />} /> */}
+
+          <Route path='/quanlysanpham' element={<QuanlySanPham />} />
+          <Route path='/sanpham/:idSanPham' element={<QuanlySanPham />} />
+          <Route path='/danhsachsanpham' element={<DanhSachSanPham />} />
+
+          <Route path='/danhsachsanphamkhuyenmai' element={<DanhSachSanPhamKM />} />
+          
+          <Route path='/sanphamkhuyenmai' element={<QuanLySanPhamKhuyenMai />} />
+          <Route path='/sanphamkhuyenmai/:idSanPhamKhuyenMai' element={<QuanLySanPhamKhuyenMai />} />
+        </Route>
+      </Route>
+    )
+  );
+
+
   return (
-    <Router>
-      <NavbarAdmin />
-      <Routes>
-        <Route path="/category-management" element={<CategoryManagement />} />
-        <Route path="/shop-approval" element={<ShopApproval />} />
-        <Route path="/" element={<CategoryManagement />} />
-        <Route path="/shop-management" element={<ShopManagement />} />
-        <Route path="/shop-register" element={<ShopRegistration />} />
-
-        <Route path="login" element={<Login />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="dangky" element={<DangKy />} />
-      </Routes>
-
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
-
-export default App;
---
+++ src/components/pages/Quanlykhuyenmai/index.js
@@ -0,0 +1,246 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { format } from "date-fns";
+const QuanLyKhuyenMai = () => {
+    const [formData, setFormData] = useState({
+        tenKhuyenMai: '',
+        soLuongKhuyenMai: '',
+        giaTriKhuyenMai: '',
+        ngayBatDau: '',
+        ngayKetThuc: '',
+        active: true,
+        ghiChu: '',
+        shop: { idShop: '' }
+    }
+    );
+    const [shopForm, setShopForm] = useState([]);
+    const { idKhuyenMai } = useParams();
+    const [edit, setEdit] = useState(true);
+
+
+
+    async function layShop() {
+        const response = await axios.get('http://localhost:8080/api/shop');
+        setShopForm(response.data);
+    }
+    async function getDataDisplayId() {
+
+        const apiKhuyenMai = 'http://localhost:8080/api/khuyenmai';
+        const response = await axios.get(apiKhuyenMai + '/' + idKhuyenMai, formData);
+        setFormData(response.data);
+    }
+
+    useEffect(() => {
+        if (idKhuyenMai) {
+            getDataDisplayId();
+            setEdit(false);
+        }
+        layShop();
+    }, [idKhuyenMai]);
+
+    function handleChange(e) {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    async function handleAdd() {
+        const dataToSent = {
+            tenKhuyenMai: formData.tenKhuyenMai,
+            soLuongKhuyenMai: formData.soLuongKhuyenMai,
+            giaTriKhuyenMai: formData.giaTriKhuyenMai,
+            ngayBatDau: formData.ngayBatDau,
+            ngayKetThuc: formData.ngayKetThuc,
+            active: true,
+            ghiChu: formData.ghiChu,
+            shop: { idShop: parseInt(formData.shop) }
+        }
+        const addData = await axios.post('http://localhost:8080/api/khuyenmai', dataToSent);
+        alert('Thêm thành công', addData.data);
+        handleResetData();
+
+
+    }
+
+    async function handleUpdate() {
+        const dataToUpdate = {
+            tenKhuyenMai: formData.tenKhuyenMai,
+            soLuongKhuyenMai: formData.soLuongKhuyenMai,
+            giaTriKhuyenMai: formData.giaTriKhuyenMai,
+            ngayBatDau: formData.ngayBatDau,
+            ngayKetThuc: formData.ngayKetThuc,
+            active: true,
+            ghiChu: formData.ghiChu,
+            shop: { idShop: parseInt(formData.shop) }
+        }
+        const apiKhuyenMai = 'http://localhost:8080/api/khuyenmai';
+        await axios.put(apiKhuyenMai + '/' + idKhuyenMai, dataToUpdate);
+        alert('Sửa thành công');
+        handleResetData();
+    }
+
+    function handleResetData() {
+        setFormData({
+            tenKhuyenMai: '',
+            soLuongKhuyenMai: '',
+            giaTriKhuyenMai: '',
+            ngayBatDau: '',
+            ngayKetThuc: '',
+            ghiChu: '',
+            shop: { idShop: '' }
+        })
+    }
+    function getFormatDate(dateString) {
+        if (!dateString) return ""; // Kiểm tra nếu không có giá trị
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) return ""; // Kiểm tra nếu không phải là ngày hợp lệ
+        return format(date, 'yyyy-MM-dd');
+    }
+
+
+
+
+    return (
+        <>
+            <main className="container my-4">
+                <div className="row">
+                    <div className="col-md-8 offset-md-2">
+                        <div className="card shadow-sm">
+                            <div className="card-header d-flex justify-content-between align-items-center">
+                                <h5 className="card-title mb-0">Quản lý Khuyến Mãi</h5>
+                                <a
+                                    className="btn btn-danger"
+                                    href="/danhsachkhuyenmai"
+                                >
+                                    Danh Sách Khuyến Mãi
+                                </a>
+                            </div>
+                            <div className="card-body">
+                                <div className="mb-3">
+                                    <label htmlFor="tenKhuyenMai" className="form-label">Tên Khuyến Mãi:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Nhập tên khuyến mãi"
+                                        name="tenKhuyenMai"
+                                        value={formData.tenKhuyenMai}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="soLuong" className="form-label">Số Lượng Khuyến Mãi:</label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        placeholder="Nhập số lượng"
+                                        name="soLuongKhuyenMai"
+                                        value={formData.soLuongKhuyenMai}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="giaTriKhuyenMai" className="form-label">Giá Trị Khuyến Mãi (Giảm giá %):</label>
+                                    <input
+                                        type="number"
+                                        className="form-control"
+                                        placeholder="Nhập giá trị"
+                                        name="giaTriKhuyenMai"
+                                        step="0.1"
+                                        value={formData.giaTriKhuyenMai}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="ngayBatDau" className="form-label">Ngày Bắt Đầu:</label>
+                                    <input
+                                        type="date"
+                                        className="form-control"
+                                        name="ngayBatDau"
+                                        value={getFormatDate(formData.ngayBatDau)}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="ngayKetThuc" className="form-label">Ngày Kết Thúc:</label>
+                                    <input
+                                        type="date"
+                                        className="form-control"
+                                        name="ngayKetThuc"
+                                        value={getFormatDate(formData.ngayKetThuc)}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="ghiChu" className="form-label">Ghi Chú:</label>
+                                    <textarea
+                                        className="form-control"
+                                        placeholder="Nhập ghi chú"
+                                        name="ghiChu"
+                                        rows="3"
+                                        value={formData.ghiChu}
+                                        onChange={handleChange}
+                                    ></textarea>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="">Shop khuyến mãi:</label>
+                                    <select
+                                        className="form-control"
+                                        name="shop"
+                                        value={formData.shop.idShop}
+                                        onChange={handleChange}
+                                    >
+                                        <option value="">Chọn Shop</option>
+                                        {shopForm.map((s) => (
+                                            <option key={s.idShop} value={s.idShop}>{s.shopName}</option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <div>
+                                    {edit ? (
+                                        <>
+                                            <button
+                                                type="button"
+                                                className="btn btn-primary me-3"
+                                                onClick={() => handleAdd()}
+                                            >
+                                                Lưu
+                                            </button>
+                                            <button
+                                                type="button"
+                                                className="btn btn-secondary"
+                                                onClick={handleResetData}
+                                            >
+                                                Hủy
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <>
+                                            <button
+                                                type="button"
+                                                className="btn btn-primary me-3"
+                                                onClick={handleUpdate}
+                                            >
+                                                Cập nhật
+                                            </button>
+                                            <button
+                                                type="button"
+                                                className="btn btn-secondary"
+                                                onClick={handleResetData}
+                                            >
+                                                Hủy
+                                            </button>
+                                        </>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+        </>
+    );
+};
+
+export default QuanLyKhuyenMai;
