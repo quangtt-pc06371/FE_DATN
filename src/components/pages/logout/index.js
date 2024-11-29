b9261947411ccc78d830d54cc44d6197175d7b20
toanphan15 <toanptpc06449@fpt.edu.vn>
2024-11-29 17:51:36 +0700

    fix

--- src/components/pages/logout/index.js
+++ src/components/pages/logout/index.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from 'react-router-dom';
+import Cookies from "js-cookie";
+// const token = Cookies.get('token'); 
 const Logout = () => {
     
-    const [, , removeCookie] = useCookies(['user','role'])
+    // const [, , removeCookie] = useCookies(['user','role'])
     console.log(typeof removeCookie);
     const navigator = useNavigate();
     const handleRemoveCookie= ()=> {
-        removeCookie('user',
-             { path: 'http://localhost:3000/' }
-            );
-            removeCookie('role',
-                { path: 'http://localhost:3000/' }
-               );
+        Cookies.remove("token")
+        Cookies.remove("refreshtoken")
+        // removeCookie('user',
+        //      { path: 'http://localhost:3000/' }
+        //     );
+        //     removeCookie('role',
+        //         { path: 'http://localhost:3000/' }
+        //        );
+        
                navigator('/')
     }
     return (
