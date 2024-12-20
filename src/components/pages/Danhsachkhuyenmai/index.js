dee7120eb4c56fb3e263a130ff34958985a25a09
tronghieu2512 <141827099+tronghieu2512@users.noreply.github.com>
2024-12-20 21:13:51 +0700

    update danhsach

--- src/components/pages/Danhsachkhuyenmai/index.js
+++ src/components/pages/Danhsachkhuyenmai/index.js
@@ -3,36 +3,90 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { format } from "date-fns";
-
-
+import Swal from "sweetalert2";
+import Cookies from "js-cookie";
+import moment from 'moment';
 export default function DanhSachkhuyenMai() {
+    const [shop, setShop] = useState(null);
+    const [dataKhuyenMai, setDataKhuyenMai] = useState([]);
 
-    const [data, setData] = useState([]);
+    async function hienThiKhuyenMai() {
+        try {
+            const apiShop = 'http://localhost:8080/api/khuyenmai/shop';
+            const response = await axios.get(apiShop + '/' + shop.id);
+            setDataKhuyenMai(response.data);
 
-    async function hienThi() {
-        const response = await axios.get('http://localhost:8080/api/khuyenmai');
-        setData(response.data);
+        } catch (error) {
+            console.log(error)
+        }
     }
 
-    function getFormatDate(dateString) {
-        const date = new Date(dateString);
-        return format(date, 'dd/MM/yyyy');
-    }
-
-    async function handleDelete(id) {
+    async function handleDeleteKhuyenMaiHetHan(id) {
         const apiKhuyenMai = 'http://localhost:8080/api/khuyenmai/updatetrangthai';
         await axios.put(apiKhuyenMai + '/' + id);
-        alert("Khuyến mãi đã được Xóa");
-        hienThi();
-      }
+
+    }
+
+    function formatDateKeepUTC(dateString) {
+        return moment.utc(dateString).format('DD/MM/YYYY');
+    }
+
+    async function handleDeleteKhuyenMai(id) {
+        // Hiển thị thông báo xác nhận trước khi xóa
+        const result = await Swal.fire({
+            title: "Bạn có chắc chắn muốn xóa khuyến mãi này?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Xóa",
+            cancelButtonText: "Hủy",
+        });
+
+        // Nếu người dùng xác nhận, thực hiện xóa
+        if (result.isConfirmed) {
+            const apiKhuyenMai = "http://localhost:8080/api/khuyenmai/updatetrangthai";
+            try {
+                await axios.put(`${apiKhuyenMai}/${id}`);
+                Swal.fire({
+                    icon: "success",
+                    title: "Khuyến mãi đã được xóa",
+                });
+                hienThiKhuyenMai();
+            } catch (error) {
+                Swal.fire({
+                    icon: "error",
+                    title: "Đã xảy ra lỗi",
+                    text: "Không thể xóa khuyến mãi. Vui lòng thử lại sau.",
+                });
+            }
+        }
+    }
 
     useEffect(() => {
-        hienThi();
+        if (shop && shop.id) {
+            hienThiKhuyenMai();
+        }
+    }, [shop]);
+
+    useEffect(() => {
+        const fetchShop = async () => {
+            const token = Cookies.get("token");
+            try {
+                const response = await axios.get('http://localhost:8080/api/shops/user', {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setShop(response.data);
+            } catch (error) {
+                console.error("Lỗi khi lấy thông tin shop:", error);
+            }
+        };
+        fetchShop();
     }, []);
 
     return (
         <div className="container my-5 d-flex justify-content-center">
-            <div className="card shadow" style={{ maxWidth: '900px', width: '100%' }}>
+            <div className="card shadow w-100 mb-5">
                 <div className="card-header bg-body-secondary d-flex justify-content-between align-items-center">
                     <h2 className="mb-0">Danh Sách Khuyến Mãi</h2>
                     <a href="/quanlykhuyenmai" className="btn btn-success">Thêm Khuyến Mãi</a>
@@ -41,36 +95,47 @@ export default function DanhSachkhuyenMai() {
                     <table className="table table-hover">
                         <thead className="table-dark">
                             <tr>
-                                <th>ID</th>
+                                <th>STT</th>
                                 <th>Tên Khuyến Mãi</th>
                                 <th>Giá Trị (%)</th>
                                 <th>Ngày Bắt Đầu</th>
                                 <th>Ngày Kết Thúc</th>
                                 <th>Ghi Chú</th>
                                 <th>Shop</th>
-                                <th>Hạn Sử Dụng</th>
+
                                 <th className="text-center">Hành Động</th>
                             </tr>
                         </thead>
                         <tbody>
-                            {data.map(khuyenMai => (
-                                khuyenMai.active === false ? null : (
+                            {dataKhuyenMai.filter(khuyenMai => khuyenMai.active !== false).map((khuyenMai, filteredIndex) => {
+                                const now = new Date();
+                                // Chuyển đổi ngày bắt đầu và kết thúc của khuyến mãi thành đối tượng Date
+                                const endDate = new Date(khuyenMai.ngayKetThuc);
+                                console.log(now);
+                                console.log(endDate);
+
+                                // Kiểm tra nếu khuyến mãi còn hạn hay hết hạn
+                                const khuyenMaiDaHetHan = now > endDate;
+                                if (khuyenMaiDaHetHan) {
+                                    handleDeleteKhuyenMaiHetHan(khuyenMai.idKhuyenMai);
+                                }
+
+                                return (
                                     <tr key={khuyenMai.idKhuyenMai}>
-                                    <td>{khuyenMai.idKhuyenMai}</td>
-                                    <td>{khuyenMai.tenKhuyenMai}</td>
-                                    <td>{khuyenMai.giaTriKhuyenMai}%</td>
-                                    <td>{getFormatDate(khuyenMai.ngayBatDau)}</td>
-                                    <td>{getFormatDate(khuyenMai.ngayKetThuc)}</td>
-                                    <td>{khuyenMai.ghiChu}</td>
-                                    <td>{khuyenMai.shop.shopName}</td>
-                                    <td>{khuyenMai.active ? "Còn hạn" : "Hết hạn"}</td>
-                                    <td className="text-center">
-                                        <div className="mb-1">                     <a className="btn btn-warning " href={`/khuyenmai/${khuyenMai.idKhuyenMai}`}>Sửa</a></div>
-                                        <button className="btn btn-danger" onClick={() => handleDelete(khuyenMai.idKhuyenMai)}>Xóa</button>
-                                    </td>
-                                </tr>
-                                )
-                            ))}
+                                        <td>{filteredIndex + 1}</td> {/* Sử dụng filteredIndex để duy trì thứ tự liên tục */}
+                                        <td>{khuyenMai.tenKhuyenMai}</td>
+                                        <td>{khuyenMai.giaTriKhuyenMai}%</td>
+                                        <td>{formatDateKeepUTC(khuyenMai.ngayBatDau)}</td>
+                                        <td>{formatDateKeepUTC(khuyenMai.ngayKetThuc)}</td>
+                                        <td>{khuyenMai.ghiChu}</td>
+                                        <td>{khuyenMai.shop.shopName}</td>
+                                        <td className="text-center">
+                                            <a className="btn btn-warning me-2" href={`/quanlykhuyenmai/${khuyenMai.idKhuyenMai}`}>Sửa</a>
+                                            <button className="btn btn-danger" onClick={() => handleDeleteKhuyenMai(khuyenMai.idKhuyenMai)}>Xóa</button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
                         </tbody>
                     </table>
                 </div>
--- src/components/pages/Danhsachsanpham/index.js
+++ src/components/pages/Danhsachsanpham/index.js
@@ -1,31 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
-
+import Swal from "sweetalert2";
+import Cookies from "js-cookie";
 const DanhSachSanPham = () => {
   const [data, setData] = useState([]);
   console.log(data)
+  const [shop, setShop] = useState(null);
+  async function hienThiSanPham() {
+    try {
+      const apiShop = 'http://localhost:8080/api/sanpham/shop';
+      const response = await axios.get(apiShop + '/' + shop.id);
+      setData(response.data);
 
-  async function hienThi() {
-    const response = await axios.get('http://localhost:8080/api/sanpham');
-    setData(response.data);
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 
-  async function handleDelete(id) {
-    const apiKhuyenMai = 'http://localhost:8080/api/sanpham/updatetrangthai';
-    await axios.put(apiKhuyenMai + '/' + id);
-    alert("Sản phẩm đã được Xóa");
-    hienThi();
+  async function handleDeleteSanPham(id) {
+    // Hiển thị thông báo xác nhận trước khi xóa
+    const result = await Swal.fire({
+      title: "Bạn có chắc chắn muốn xóa sản phẩm này?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Xóa",
+      cancelButtonText: "Hủy",
+    });
+
+    // Nếu người dùng xác nhận, thực hiện xóa
+    if (result.isConfirmed) {
+      const apiSanPham = "http://localhost:8080/api/sanpham/updatetrangthai";
+      try {
+        await axios.put(`${apiSanPham}/${id}`);
+        Swal.fire({
+          icon: "success",
+          title: "Xóa sản phẩm thành công",
+        });
+        hienThiSanPham(); // Load lại danh sách sản phẩm sau khi xóa
+      } catch (error) {
+        Swal.fire({
+          icon: "error",
+          title: "Đã xảy ra lỗi",
+          text: "Không thể xóa sản phẩm. Vui lòng thử lại sau.",
+        });
+      }
+    }
   }
 
 
   useEffect(() => {
-    hienThi();
+    if (shop && shop.id) {
+      hienThiSanPham();
+
+    }
+  }, [shop]);
+  console.log(shop)
+
+  useEffect(() => {
+    const fetchShop = async () => {
+      const token = Cookies.get("token");
+      try {
+        const response = await axios.get('http://localhost:8080/api/shops/user', {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setShop(response.data);
+      } catch (error) {
+        console.error("Lỗi khi lấy thông tin shop:", error);
+      }
+    };
+    fetchShop();
   }, []);
+
 
   return (
     <div className="container-fluid my-5">
-      <div className="card shadow w-100">
+      <div className="card shadow-sm w-100 mb-5">
         <div className="card-header bg-body-secondary d-flex justify-content-between align-items-center">
           <h2>Danh Sách Sản Phẩm</h2>
           <a href='/quanlysanpham' className="btn btn-success">Thêm Sản Phẩm</a>
@@ -34,7 +87,7 @@ const DanhSachSanPham = () => {
           <table className="table table-hover">
             <thead className="table-dark">
               <tr>
-                <th>ID</th>
+                <th>STT</th>
                 <th>Tên Sản Phẩm</th>
                 <th>Mô Tả</th>
                 <th>Shop</th>
@@ -44,39 +97,21 @@ const DanhSachSanPham = () => {
               </tr>
             </thead>
             <tbody>
-              {data.map(sanPham => (
-                // Kiểm tra nếu sản phẩm có trong hiddenProducts thì không hiển thị
-                sanPham.trangThai === false ? null : (
-                  <tr key={sanPham.idSanPham}>
-                    <td>{sanPham.idSanPham}</td>
-                    <td>{sanPham.tenSanPham}</td>
-                    <td>{sanPham.moTa}</td>
-                    <td>{sanPham.shop.shopName}</td>
-                    <td>{sanPham.danhMuc.tenDanhMuc}</td>
-                    <td>
-                      Số lượng phiên bản: {sanPham.skus.length}
-                      {sanPham.skus.length > 0 && (
-                        <ul className="list-unstyled">
-                          <li>
-                            <ul>
-                              <li>Giá: {sanPham.skus[0].giaSanPham} VNĐ</li>
-                              <li>Số lượng: {sanPham.skus[0].soLuong}</li>
-                              {sanPham.skus[0].tuyChonThuocTinhSkus.map(tcSkus => (
-                                <li key={tcSkus.idtuyChonThuocTinhSku}>
-                                  {tcSkus.tuyChonThuocTinh.thuocTinh.ten}: {tcSkus.tuyChonThuocTinh.giaTri}
-                                </li>
-                              ))}
-                            </ul>
-                          </li>
-                        </ul>
-                      )}
-                    </td>
-                    <td>
-                      <a href={`/sanpham/${sanPham.idSanPham}`} className="btn btn-warning me-2">Sửa</a>
-                      <button onClick={() => handleDelete(sanPham.idSanPham)} className="btn btn-danger">Xóa</button>
-                    </td>
-                  </tr>
-                )
+              {data.filter(sanPham => sanPham.trangThai !== false).map((sanPham, index) => (
+                <tr key={sanPham.idSanPham}>
+                  <td>{index + 1}</td> {/* Duy trì chỉ số với sản phẩm không ẩn */}
+                  <td>{sanPham.tenSanPham}</td>
+                  <td>{sanPham.moTa}</td>
+                  <td>{sanPham.shop.shopName}</td>
+                  <td>{sanPham.danhMuc.tenDanhMuc}</td>
+                  <td>
+                    Số lượng phiên bản: {sanPham.skus.length}
+                  </td>
+                  <td>
+                    <a href={`/sanpham/${sanPham.idSanPham}`} className="btn btn-warning me-2">Sửa</a>
+                    <button onClick={() => handleDeleteSanPham(sanPham.idSanPham)} className="btn btn-danger">Xóa</button>
+                  </td>
+                </tr>
               ))}
             </tbody>
           </table>
